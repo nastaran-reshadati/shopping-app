d545771948fb5a0a4b55cd89a4351b4ba6ea3ccb
nastaran reshadati <96713994+nastaran-reshadati@users.noreply.github.com>
2024-07-08 09:13:59 +0330

    add card section

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="light">
           <Header />
+
           {children}
         </ThemeProvider>
       </body>
---
+++ components/card/page.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <section className="container mx-auto my-14">
+      <div className="grid sm:grid-cols-1 md:grid-cols-3  gap-3">
+        <div className="[&>*]:font-DanaMedium rounded-3xl py-2 px-3 bg-transparent shadow-md dark:bg-gray-700">
+          <div className="flex justify-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-6 cursor-pointer w-8 h-8"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
+              />
+            </svg>
+          </div>
+          <h3 className="text-center mt-3 mb-4"> تماس سریع</h3>
+          <p className="leading-8">
+            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+            استفاده از طراحان گرافیک است.
+          </p>
+        </div>
+        <div className="[&>*]:font-DanaMedium rounded-3xl py-2 px-3 bg-transparent shadow-md dark:bg-gray-700">
+          <div className="flex justify-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6 cursor-pointer w-8 h-8"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+          </div>
+          <h3 className="text-center mt-3 mb-4"> 24 ساعت شبانه روز </h3>
+          <p className="leading-8">
+            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+            استفاده از طراحان گرافیک است.
+          </p>
+        </div>
+        <div className="[&>*]:font-DanaMedium rounded-3xl py-2 px-3 bg-transparent shadow-md dark:bg-gray-700">
+          <div className="flex justify-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6 cursor-pointer w-8 h-8"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+              />
+            </svg>
+          </div>
+          <h3 className="text-center mt-3 mb-4">لوکیشن جذاب</h3>
+          <p className="leading-8">
+            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+            استفاده از طراحان گرافیک است.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Card;
