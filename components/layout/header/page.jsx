813221a8b201fa434e14ac784899ef34eb4a34f4
nastaran reshadati <96713994+nastaran-reshadati@users.noreply.github.com>
2024-07-18 23:42:32 +0330

    responsive menu

--- components/layout/header/page.jsx
+++ components/layout/header/page.jsx
@@ -17,6 +17,8 @@ const Header = () => {
     setTheme(theme === "light" ? "dark" : "light");
   };
 
+  console.log(theme);
+
   const handleClick = (index) => {
     setCurrentIndex(index);
   };
@@ -43,10 +45,12 @@ const Header = () => {
       <div className="relative">
         <Image src={heroImage} priority alt="hero-image" height="805px" />
       </div>
-      <header className="fixed top-8 right-0 left-0 w-[90%] h-24 z-50 rounded-3xl py-5 px-10 mx-auto flex bg-blue justify-between items-center bg-black/50 backdrop-blur-[6px] text-brown-300">
+
+      {/* Desktop menu */}
+      <header className="fixed top-8 right-0 left-0 w-[95%] lg:w-[90%] h-24 z-50 rounded-3xl py-5 px-10 mx-auto hidden md:flex bg-blue justify-between items-center bg-black/50 backdrop-blur-[6px] text-brown-300">
         {/* logo & menu */}
         <nav className="flex items-center gap-x-9">
-          <ul className="flex flex-row gap-x-9 child:text-gray-300 font-Dana  text-xl tracking-tightest child-hover:text-orange-400">
+          <ul className="flex flex-row gap-x-6 lg:gap-x-9 child:text-gray-300 font-Dana text-lg lg:text-xl tracking-tightest child-hover:text-orange-400">
             <li>
               <Link
                 href="/"
@@ -75,8 +79,8 @@ const Header = () => {
             </li>
           </ul>
         </nav>
-        <div className="flex gap-x-10">
-          <div className="flex items-center gap-x-5 py-7">
+        <div className="flex gap-x-5 lg:gap-x-10 items-center">
+          <div className="flex items-center gap-x-4 py-7">
             <div>
               <svg className="svg-icon" stroke="currentColor">
                 <use href="/icons.svg#shopping-bag" />
@@ -104,7 +108,7 @@ const Header = () => {
 
             <Link
               href={"/auth/login"}
-              className="font-Dana text-xl tracking-tightest"
+              className="hidden lg:inline-block font-Dana text-xl tracking-tightest"
             >
               ورود | ثبت نام
             </Link>
@@ -113,9 +117,128 @@ const Header = () => {
         {/* Card & theme toggele  */}
         {/* <div>left section</div> */}
       </header>
+
+      {/* Mobile  Navbar   */}
+      <section>
+        <div className="fixed top-0 right-0 w-full flex md:hidden bg-gray-100 dark:bg-black/20 backdrop-blur-[6px]">
+          <div className="flex w-full  items-center justify-between h-16 px-5">
+            <svg className="svg-icon" stroke="currentColor">
+              <use href="/icons.svg#bars-3" />
+            </svg>
+            <svg>
+              <use href="/icons.svg#shop" />
+            </svg>
+            <svg className="svg-icon" stroke="currentColor">
+              <use href="/icons.svg#shopping-bag" />
+            </svg>
+          </div>
+        </div>
+        {/* Mobile Menu  */}
+        <div className="md:hidden w-64 min-h-screen pr-7 pl-4 pt-5 fixed top-0 bottom-0 right-0 bg-white dark:bg-zinc-700 z-50">
+          <div className="flex justify-end">
+            <svg stroke="currentColor" className="w-5 h-5">
+              <use href="/icons.svg#x-mark" />
+            </svg>
+          </div>
+
+          <ul className="flex flex-col justify-between gap-y-6 pb-8 mt-5 border-b border-b-gray-300/50  dark:border-b-white/10 lg:gap-x-9 text-zinc-600 dark:text-white font-Dana  tracking-tightest child-hover:text-orange-400">
+            <li
+              className={`${
+                currentPath === "/" && "active-responsive-link"
+              } flex gap-x-2 pr-2 py-[9px]`}
+            >
+              <svg stroke="currentColor" className="w-5 h-5">
+                <use href="/icons.svg#home" />
+              </svg>
+              <Link href="/">صفحه اصلی</Link>
+            </li>
+            <li
+              className={`${
+                currentPath === "/menu" && "active-responsive-link"
+              } flex gap-x-2 pr-2 py-[9px]`}
+            >
+              <svg stroke="currentColor" className="w-5 h-5">
+                <use href="/icons.svg#bell-alert" />
+              </svg>
+              <Link
+                href="/menu"
+                className={currentPath === "/menu" ? "active-link" : null}
+              >
+                منو
+              </Link>
+            </li>
+
+            <li
+              className={`${
+                currentPath === "/blog" && "active-responsive-link"
+              } flex gap-x-2 pr-2 py-[9px]`}
+            >
+              <svg stroke="currentColor" className="w-5 h-5">
+                <use href="/icons.svg#document" />
+              </svg>
+              <Link href="/blog">بلاگ</Link>
+            </li>
+            <li
+              className={`${
+                currentPath === "/aboutUs" && "active-responsive-link"
+              } flex gap-x-2 pr-2 py-[9px]`}
+            >
+              <svg stroke="currentColor" className="w-5 h-5">
+                <use href="/icons.svg#building-office" />
+              </svg>
+              <Link href="/aboutUs">درباره ما</Link>
+            </li>
+            <li
+              className={`${
+                currentPath === "/call" && "active-responsive-link"
+              } flex gap-x-2 pr-2 py-[9px]`}
+            >
+              <svg stroke="currentColor" className="w-5 h-5">
+                <use href="/icons.svg#phone-arrow-down-left" />
+              </svg>
+              <Link href="address">تماس با ما</Link>
+            </li>
+          </ul>
+
+          <div className="font-Dana space-y-4 pt-8 child:cursor-pointer child:text-orange-300">
+            <div className="inline-flex items-center gap-3">
+              <svg stroke="currentColor" className="w-5 h-5">
+                <use href="/icons.svg#login" />
+              </svg>
+
+              <Link href={"/auth/login"} className="tracking-tightest">
+                ورود | ثبت نام
+              </Link>
+            </div>
+            <div className="flex items-center gap-x-2" onClick={toggleTheme}>
+              {theme === "light" ? (
+                <svg stroke="currentColor" className="w-5 h-5">
+                  <use href="/icons.svg#moon" />
+                </svg>
+              ) : (
+                <svg stroke="currentColor" className="w-5 h-5">
+                  <use href="/icons.svg#sun" />
+                </svg>
+              )}
+              <span className="tracking-tightest">
+                {theme === "light" ? "تیره" : "روشن"}
+              </span>
+            </div>
+
+            <div className="inline-flex items-center gap-3">
+              {" "}
+              <svg stroke="currentColor" className="w-5 h-5">
+                <use href="/icons.svg#shopping-bag" />
+              </svg>
+              <span className="tracking-tightest">سبد خرید</span>
+            </div>
+          </div>
+        </div>
+      </section>
+
       <div
         id="default-carousel"
-        className="absolute top-[11rem] right-36 container"
+        className="container absolute top-[11rem] right-0 px-8 mx-auto  overflow-hidden"
         data-carousel="slide"
       >
         <div className="duration-700 ease-in-out">
